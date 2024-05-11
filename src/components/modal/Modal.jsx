d914cec6a2719954bfe30cfd207c663e08528ce1
Tareq <tareqnmd@gmail.com>
2024-05-11 14:36:12 +0600

    modal added

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,36 +1,19 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import { PropTypes } from 'prop-types';
-import styled from 'styled-components';
 import './modal.css';
-const ModalDiv = styled(motion.div)`
-    position: fixed;
-	z-index: 9999;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	background-color: white;
-`
-const BackDrop = styled(motion.div)`
-    background: #aaaaaa2e;
-	position: fixed;
-	top: 0;
-	bottom: 0;
-	right: 0;
-	left: 0;
-	z-index: 9998;
-`
+
 const Modal = ({ open, title, children, onCancel, footer, extraClassName }) => {
     return (
         <AnimatePresence>
             {open &&
                 <>
-                    <BackDrop onClick={onCancel} />
-                    <ModalDiv
+                    <motion.div className='modal-backdrop' onClick={onCancel} />
+                    <motion.div
+                        className={`modal-container ${extraClassName ?? ''}`}
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
                         transition={{ duration: 0.3 }}
-                        className={extraClassName}
                     >
                         <div className="modal-title">
                             <h4>{title}</h4>
@@ -44,7 +27,7 @@ const Modal = ({ open, title, children, onCancel, footer, extraClassName }) => {
                         {footer ? <div className="modal-footer">
                             {footer}
                         </div> : null}
-                    </ModalDiv>
+                    </motion.div>
                 </>
             }
         </AnimatePresence>
