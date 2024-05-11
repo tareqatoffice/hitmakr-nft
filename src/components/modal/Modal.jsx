9bddfe497043743be25bfc40cc335f56712d69d2
Tareq <tareqnmd@gmail.com>
2024-05-11 14:37:17 +0600

    modal added

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -17,7 +17,7 @@ const Modal = ({ open, title, children, onCancel, footer, extraClassName }) => {
                     >
                         <div className="modal-title">
                             <h4>{title}</h4>
-                            <button>
+                            <button onClick={onCancel}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
                             </button>
                         </div>
