567c43e4af271a394b0ebb0450640ef065e47050
Tareq <tareqnmd@gmail.com>
2024-05-11 14:57:17 +0600

    modal

--- src/components/nft/NFTArea.jsx
+++ src/components/nft/NFTArea.jsx
@@ -1,33 +1,76 @@
-import { useState } from "react";
-import { nftCards } from "../../lib/nft-card";
-import Button from "../common/Button";
-import Modal from "../modal/Modal";
-import NFTCard from "./NFTCard";
-import SuccessMessage from "./SuccessMessage";
+import { AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
+import { nftCards } from '../../lib/nft-card';
+import Button from '../common/Button';
+import Modal from '../modal/Modal';
+import NFTCard from './NFTCard';
+import SuccessMessage from './SuccessMessage';
 import './nft-card.css';
 const NFTArea = () => {
-    const clickHandler = () => { setIsOpen(prev => !prev) }
     const [isOpen, setIsOpen] = useState(false);
+    const [isAnimating, setIsAnimating] = useState(false);
+    const clickHandler = () => {
+        setIsAnimating(true);
+        setTimeout(() => {
+            setIsOpen(prev => !prev)
+        }, 300);
+    };
     const onCancel = () => {
         setIsOpen(false);
+        setTimeout(() => {
+            setIsAnimating(false)
+        }, 300)
+
     };
     const onOk = () => {
         setIsOpen(false);
+        setTimeout(() => {
+            setIsAnimating(false)
+        }, 300)
     };
     return (
         <>
-            <section className="nft-card-area">
-                <h3 className="nft-card-title">Popular Artworks</h3>
-                <div className="nft-cards grid">
-                    {nftCards.map((nft, index) => <NFTCard key={index} nft={nft} />)}
-                </div>
-                <Button onClick={clickHandler} extraClassName='w-max mx-auto'>Buy NFT</Button>
-            </section>
-            <Modal open={isOpen} title={'Successfully NFT Buy'} onCancel={onCancel} onOk={onOk} extraClassName='sm:w-[50%]' footer={
-                <Button extraClassName="w-full" onClick={onOk}>Ok, Got It.</Button>
-            }>
+            <AnimatePresence>
+                <section className="nft-card-area">
+                    <h3 className="nft-card-title">Popular Artworks</h3>
+                    <div className="nft-cards grid">
+                        {nftCards.map((nft, index) => (
+                            <NFTCard
+                                key={index}
+                                nft={nft}
+                                isCenter={index === 1}
+                                isAnimating={isAnimating}
+                            />
+                        ))}
+                    </div>
+                    <Button
+                        onClick={clickHandler}
+                        extraClassName="w-max mx-auto"
+                    >
+                        Buy NFT
+                    </Button>
+                </section>
+            </AnimatePresence>
+            <Modal
+                open={isOpen}
+                title={'Successfully NFT Buy'}
+                onCancel={onCancel}
+                onOk={onOk}
+                extraClassName="sm:w-[50%]"
+                footer={
+                    <Button
+                        extraClassName="w-full"
+                        onClick={onOk}
+                    >
+                        Ok, Got It.
+                    </Button>
+                }
+            >
                 <div className="success-info">
-                    <NFTCard small={true} nft={nftCards[0]} />
+                    <NFTCard
+                        small={true}
+                        nft={nftCards[0]}
+                    />
                     <SuccessMessage nft={nftCards[0]} />
                 </div>
             </Modal>
--- src/components/nft/NFTCard.jsx
+++ src/components/nft/NFTCard.jsx
@@ -1,21 +1,40 @@
+import { motion } from 'framer-motion';
 import PropTypes from 'prop-types';
 import NFTCardInfo from './NFTCardInfo';
 import NFTMoney from './NFTMoney';
-
-const NFTCard = ({ nft, small = false }) => {
+const NFTCard = ({ nft, small = false, isCenter = true, isAnimating = false }) => {
     const { coverImg, nftInfo, nfts } = nft || {};
     return (
-        <div className={`nft-card-details ${small ? 'small-card' : ''}`}>
-            <img className='nft-card-cover' src={coverImg} alt="" />
+        <motion.div
+            initial={{ scale: 1, opacity: 1 }}
+            animate={isAnimating ? {
+                scale: isCenter ? 1.1 : (isAnimating && !isCenter) ? 0 : 0.8,
+                opacity: isCenter ? 1 : (isAnimating && !isCenter) ? 0 : 0.5,
+            } : null}
+            transition={{ duration: 0.3 }}
+            className={`nft-card-details ${small ? 'small-card' : ''}`}
+        >
+            <img
+                className="nft-card-cover"
+                src={coverImg}
+                alt=""
+            />
             <NFTCardInfo nftInfo={nftInfo} />
             <div className="nfts">
-                {nfts.map((nftMoney, index) => <NFTMoney key={index} nftMoney={nftMoney} />)}
+                {nfts.map((nftMoney, index) => (
+                    <NFTMoney
+                        key={index}
+                        nftMoney={nftMoney}
+                    />
+                ))}
             </div>
-        </div>
+        </motion.div>
     );
 };
 NFTCard.propTypes = {
     nft: PropTypes.object,
-    small: PropTypes.boolean
+    isCenter: PropTypes.boolean,
+    isAnimating: PropTypes.boolean,
+    small: PropTypes.boolean,
 };
 export default NFTCard;
