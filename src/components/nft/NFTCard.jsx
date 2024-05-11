dc8ddd7d0833a701586b9b14a01c728bfcf19c43
Tareq <tareqnmd@gmail.com>
2024-05-11 16:13:03 +0600

    animation updated

--- src/components/nft/NFTCard.jsx
+++ src/components/nft/NFTCard.jsx
@@ -5,6 +5,7 @@ import NFTCardInfo from './NFTCardInfo';
 import NFTMoney from './NFTMoney';
 const NFTCard = ({ nft, small = false, index = 1, isAnimating = false }) => {
     const cardRef = useRef(null);
+    const windowWidth = window?.outerWidth ?? 0;
     const { coverImg, nftInfo, nfts } = nft || {};
     return (
         <motion.div
@@ -12,7 +13,8 @@ const NFTCard = ({ nft, small = false, index = 1, isAnimating = false }) => {
             initial={{ scale: 1, opacity: 1 }}
             animate={isAnimating ? {
                 filter: index === 1 ? null : "blur(5px)",
-                x: index === 0 ? cardRef.current.offsetWidth + 16 : index === 2 ? -cardRef.current.offsetWidth - 16 : null,
+                x: (index === 0 && windowWidth >= 991) ? cardRef.current.offsetWidth + 16 : (index === 2 && windowWidth >= 991) ? - cardRef.current.offsetWidth - 16 : null,
+                y: (index === 0 && windowWidth < 991) ? cardRef.current.offsetHeight + 16 : (index === 2 && windowWidth < 991) ? - cardRef.current.offsetHeight - 16 : null,
             } : null}
             transition={{ duration: 0.3 }}
             className={`nft-card-details ${index === 1 ? 'z-[10]' : 'z-[9]'} ${small ? 'small-card' : ''}`}
